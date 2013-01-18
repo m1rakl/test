54e54350d80c6d731ee47028bfe18f0323f65c61
m1rakl <email@m1rakl.com>
2013-01-18 18:45:03 +0200

    + Adapter

---
+++ patterns/Structural/Adapter/index.php
@@ -0,0 +1,43 @@
+<?php
+
+class Search
+{
+    protected $word;
+    protected $text;
+
+    public function __construct($word, $text)
+    {
+        $this->word = $word;
+        $this->text = $text;
+    }
+
+    public function getSearchWord()
+    {
+        return $this->word;
+    }
+
+    public function getSearchText()
+    {
+        return $this->text;
+    }
+}
+
+class SearchAdapter
+{
+    protected $search;
+
+    public function __construct(Search $search)
+    {
+        $this->search = $search;
+    }
+
+    public function getSearchWordInText()
+    {
+        echo 'This word "' . $this->search->getSearchWord() . '" was found in text : ' . $this->search->getSearchText();
+    }
+}
+
+$search = new Search('some', 'some text');
+
+$searchAdapter = new SearchAdapter($search);
+$searchAdapter->getSearchWordInText();
