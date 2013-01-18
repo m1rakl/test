1c04e010e13f61bb0a0f47b2f3e01f0d0be7ef18
m1rakl <email@m1rakl.com>
2013-01-18 18:47:42 +0200

    - update Adapter

--- patterns/Structural/Adapter/index.php
+++ patterns/Structural/Adapter/index.php
@@ -33,11 +33,11 @@ class SearchAdapter
 
     public function getSearchWordInText()
     {
-        echo 'This word "' . $this->search->getSearchWord() . '" was found in text : ' . $this->search->getSearchText();
+        return 'This word "' . $this->search->getSearchWord() . '" was found in text : ' . $this->search->getSearchText();
     }
 }
 
 $search = new Search('some', 'some text');
 
 $searchAdapter = new SearchAdapter($search);
-$searchAdapter->getSearchWordInText();
+echo $searchAdapter->getSearchWordInText();
