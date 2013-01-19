45b16677e8e653b4fe2fc767266b37d85e6f5a55
m1rakl <email@m1rakl.com>
2013-01-19 17:56:28 +0200

    + Decorator

---
+++ patterns/Structural/Decorator/index.php
@@ -0,0 +1,43 @@
+<?php
+
+abstract class AbstractComponent
+{
+    abstract public function doAction();
+}
+
+class ConcreteComponent extends AbstractComponent
+{
+    public function doAction()
+    {
+        echo 'some action';
+    }
+}
+
+abstract class AbstractDecorator extends AbstractComponent
+{
+    protected $component;
+
+    public function __construct(AbstractComponent $component)
+    {
+        $this->component = $component;
+    }
+
+    public function doAction()
+    {
+        $this->component->doAction();
+    }
+}
+
+class ConcreteDecorator extends AbstractDecorator
+{
+    public function doAction()
+    {
+        echo ':start action: ';
+        parent::doAction();
+        echo ' :end action:';
+    }
+}
+
+$decorateComponent = new ConcreteDecorator(new ConcreteComponent());
+$decorateComponent->doAction();
+
